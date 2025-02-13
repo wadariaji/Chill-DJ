6965f484c0f741b12eae04d8f511cb145a089a8e
wadariaji <garrybhullar50071@gmail.com>
2025-02-13 18:13:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://wadaria:wadaria@cluster0.kf2od.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
