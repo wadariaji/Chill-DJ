9ea6bbfb8999bf06038f3bf856e63afd01835fdf
wadariaji <garrybhullar50071@gmail.com>
2025-02-14 11:12:29 +0530

    Update en.js

--- languages/en.js
+++ languages/en.js
@@ -1,5 +1,5 @@
 module.exports = {
-    footer: "Developed by SSRR | Prime Music v1.2",
+    footer: "Developed by justxn_u | Chill DJ",
     ping: {
       description: "Check the bot latency",
       response: "Pinging...",
@@ -8,7 +8,7 @@ module.exports = {
         responseTime: "- Bot Response Time : **{latency}ms**",
         websocketPing: "- WebSocket Ping : **{ping}ms**",
         uptime: "- Uptime : **{uptime}**",
-        footer: "Developed by SSRR | Prime Music v1.2"
+        footer: "Developed by justxn_u | Chill DJ"
       }
     },
     addsong: {
