4c827b0bf7326437e89c4233f3d620b404188ebb
wadariaji <garrybhullar50071@gmail.com>
2025-03-01 21:30:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "Private",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port: 7019,
+      name: "ChalresNaig Node",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port: 3000,
       secure: false
     }
   ]
